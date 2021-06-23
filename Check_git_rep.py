0a48a18c569953ac49b55c91b724fbc66753d575
Urli_by <urli.by@gmail.com>
2021-06-23 16:38:44 +0300

    edited to markdown 0.2

---
+++ Check_git_rep.py
@@ -0,0 +1,10 @@
+import os
+
+bash_command = ["cd C:/Users/User/PycharmProjects/devops-netology", "git status"]
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = False
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
+        break
