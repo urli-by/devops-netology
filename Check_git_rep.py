7cca483cd0badbcbdff3afb0d6e7d52e052e4515
Urli_by <urli.by@gmail.com>
2021-06-23 19:47:00 +0300

    updated py

--- Check_git_rep.py
+++ Check_git_rep.py
@@ -1,9 +1,13 @@
 import os
 
-bash_command = ["cd C:/Users/User/PycharmProjects/devops-netology", "git status"]
+bash_command = ["cd ./", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
+current_directory = os.getcwd()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
-        print(prepare_result)
-        break
+        print('Modified:', current_directory + '\\' + prepare_result)
+    elif result.find('new') != -1:
+        prepare_result = result.replace('\tnew file:   ', '')
+        print('New file:', current_directory + '\\' + prepare_result)
+
