eb0e8affe2b70853f4a356e2f140eb67a37e1558
Urli_by <urli.by@gmail.com>
2021-06-23 20:01:45 +0300

    updated py

--- Check_git_rep.py
+++ Check_git_rep.py
@@ -3,11 +3,16 @@ import os
 bash_command = ["cd ./", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 current_directory = os.getcwd()
+counter = 0
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print('Modified:', current_directory + '\\' + prepare_result)
+        counter += 1
     elif result.find('new') != -1:
         prepare_result = result.replace('\tnew file:   ', '')
         print('New file:', current_directory + '\\' + prepare_result)
-        exit('all done')
+        counter += 1
+if counter == 0:
+    print('nothing changed')
+
